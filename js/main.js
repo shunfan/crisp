9032733f18d1fddeb6e90f7b860eff07b556ecaf
Perry Du <i@perry.asia>
2013-04-24 20:08:18 +0800

    3rd commit.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$("#header h1 a").hover(function(){
+    $(this).css('letter-spacing', '10px');
+});
+
+$("#github").click(function() {
+    $("#github-buttons").fadeToggle("slow", "swing");
+});
+
+$("#random").click(function() {
+    var hue = 'rgb('
+                + (Math.floor(Math.random() * 256)) + ','
+                + (Math.floor(Math.random() * 256)) + ','
+                + (Math.floor(Math.random() * 256)) + ')';
+    less.modifyVars({
+        '@general': hue,
+    });
+});
